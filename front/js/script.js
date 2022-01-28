63943a4d6358fb4ffd11cd382385a29aae3ef29b
Hata Couibaly <hata.coulibaly@yahoo.fr>
2022-01-28 21:36:51 +0100

    Add products in homepage

---
+++ front/js/script.js
@@ -0,0 +1,77 @@
+// Section where items will be added dynamically  
+const $items = document.querySelector('#items')
+
+// Fetch request to get product items
+const retrieveItemsData = () => fetch("http://localhost:3000/api/products")
+    .then(res => res.json())
+    .catch(err => console.log("What's happening ?", err))
+
+
+// Create card item
+const createItemCard = item => {
+    const $itemCard = document.createElement('a')
+    // ajouter un attribut href avec l'id du produit 
+    $itemCard.setAttribute('href', `../html/product.html?id=${item._id}`)
+    const $itemCardArt = createItemCardArt(item)
+    $itemCard.appendChild($itemCardArt)
+    
+    return $itemCard
+} 
+
+// Create article tag which contains img, name and description
+const createItemCardArt = item => {
+    const $itemCardArt = document.createElement('article')
+
+    const $itemImg = createItemCardImg(item)
+    const $itemName = createItemCardName(item)
+    const $itemDesc = createItemCardDesc(item)
+    // Add img and info
+    $itemCardArt.appendChild($itemImg)
+    $itemCardArt.appendChild($itemName)
+    $itemCardArt.appendChild($itemDesc)
+
+    return $itemCardArt
+} 
+
+
+// Create variables to get img, name and description
+const createItemCardImg = item => {
+    const $itemImg = document.createElement('img')
+    $itemImg.setAttribute('src', `${item.imageUrl}`)
+    $itemImg.setAttribute('alt', `${item.altTxt}`)
+
+    return $itemImg
+}
+
+const createItemCardName = item => {
+    
+    const $itemName = document.createElement('h3')
+    $itemName.classList.add('productName')
+    $itemName.textContent = `${item.name}`
+
+    return $itemName
+}
+
+const createItemCardDesc = item => {
+    
+    const $itemDesc = document.createElement('p')
+    $itemDesc.classList.add('productDescription')
+    $itemDesc.textContent = `${item.description}`
+
+    return $itemDesc
+}
+
+
+
+// Loop for used to create item card from the get request
+const main = async () => {
+    const itemsData = await retrieveItemsData()
+
+    for (let i = 0; i < itemsData.length; i++) {
+        if (itemsData[i]) {
+            $items.appendChild(createItemCard(itemsData[i]))
+        }
+    }
+}
+
+main()
